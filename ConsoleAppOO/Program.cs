02ce412ef849a6abbe3579088a13119dc48f2acd
Heitor Albuquerque <heitor.albuquerque@gmail.com>
2020-09-15 11:07:47 -0300

    terceiro commit

--- ConsoleAppOO/Program.cs
+++ ConsoleAppOO/Program.cs
@@ -49,9 +49,9 @@ namespace ConsoleAppOO
                         catch (Exception ex)
                         {
                             Console.WriteLine("ocorreu algum problema, por favor reinicie o procedimento");
+                            Console.ReadKey();
                             break;
                         }
-
 
                     case 2:
                         foreach (var item in repositorio.consultarTodasPessoas())
@@ -63,90 +63,110 @@ namespace ConsoleAppOO
 
 
                     case 3:
-                        Console.WriteLine("digite o número da pessoa que você deseja candidatar :  ");
-
-                        var listapessoas = repositorio.consultarTodasPessoas();
-
-                        var cont = 0;
-                        foreach (var item in repositorio.consultarTodasPessoas())
+                        try
                         {
+                            Console.WriteLine("digite o número da pessoa que você deseja candidatar :  ");
+                            var listapessoas = repositorio.consultarTodasPessoas();
+                            var cont = 0;
 
-                            Console.WriteLine("( " + cont + " )" + listapessoas[cont].getNome());
-                            cont++;
-                        }
-
-                        var choice = Convert.ToInt32(Console.ReadLine());
-
-                        Pessoa pessoaEscolhida = listapessoas[choice];
-
-                        Console.Clear();
-
-                        Console.WriteLine("Ok, você escolheu o (a) " + pessoaEscolhida.getNome() + " para ser candidato");
-                        Console.ReadKey();
-                        Console.WriteLine("Digite o nome do partido");
-
-                        var partidoEscolhido = Console.ReadLine();
-                        Console.Clear();
-                        Console.WriteLine("Ok, Agora digite o número do candidato : ");
-                        var numeroDoPartido = Convert.ToInt32(Console.ReadLine());
-                        foreach (var item in repositorio.consultarTodosCandidatos())
-                        {
-                            if (numeroDoPartido == item.getNumeroPartido())
+                            foreach (var item in repositorio.consultarTodasPessoas())
                             {
-                                Console.WriteLine("este número já existe, escolha outro");
-                                Console.ReadKey();
-                                break;
+                                Console.WriteLine("( " + cont + " )" + listapessoas[cont].getNome());
+                                cont++;
                             }
+
+                            var choice = Convert.ToInt32(Console.ReadLine());
+                            Pessoa pessoaEscolhida = listapessoas[choice];
+
+                            Console.Clear();
+                            Console.WriteLine("Ok, você escolheu o (a) " + pessoaEscolhida.getNome() + " para ser candidato");
+                            Console.ReadKey();
+                            Console.WriteLine("Digite o nome do partido");
+
+                            var partidoEscolhido = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Ok, Agora digite o número do candidato : ");
+                            var numeroDoPartido = Convert.ToInt32(Console.ReadLine());
+
+                            foreach (var item in repositorio.consultarTodosCandidatos())
+                            {
+                                if (numeroDoPartido == item.getNumeroPartido())
+                                {
+                                    //Console.WriteLine("este número já existe, escolha outro");
+                                    Console.ReadKey();
+                                    throw new Exception("este número já existe, escolha outro");
+
+                                }
+                            }
+
+                            Candidato novoCandidato = new Candidato(pessoaEscolhida, partidoEscolhido, numeroDoPartido);
+
+                            repositorio.adicionarCandidato(novoCandidato);
+                            Console.Clear();
+
+                            Console.WriteLine("Candidato adicionado!");
+                            break;
                         }
-
-                        Candidato novoCandidato = new Candidato(pessoaEscolhida, partidoEscolhido, numeroDoPartido);
-
-                        repositorio.adicionarCandidato(novoCandidato);
-                        Console.Clear();
-
-                        Console.WriteLine("Candidato adicionado!");
-                        break;
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            Console.ReadKey();
+                            break;
+                        }
 
                     case 4:
                         foreach (var item in repositorio.consultarTodosCandidatos())
                         {
-                            Console.WriteLine(item.getNome() + ", " + item.getIdade() + " Anos, Partido : " + item.getPartido() + "Total de votos : " + item.getQtdVotos());
+                            Console.WriteLine(item.getNome() + ", " + item.getIdade() + " Anos, Partido : " + item.getPartido() + " Total de votos : " + item.getQtdVotos());
                         }
                         Console.ReadKey();
                         break;
 
                     case 5:
-                        Console.Clear();
-                        Console.WriteLine("Escolha um candidato pelo seu número");
-                        var listaCandidato = repositorio.consultarTodosCandidatos();
-                        foreach (var item in listaCandidato)
+                        try
                         {
-                            Console.WriteLine(item.getNome() + ", ( " + item.getNumeroPartido() + " )");
+                            Console.Clear();
+                            Console.WriteLine("Escolha um candidato pelo seu número");
+                            var listaCandidato = repositorio.consultarTodosCandidatos();
+                            foreach (var item in listaCandidato)
+                            {
+                                Console.WriteLine(item.getNome() + ", ( " + item.getNumeroPartido() + " )");
+                            }
+
+                            var escolhaVoto = Convert.ToInt32(Console.ReadLine());
+                            if (!listaCandidato.Select(p => p.getNumeroPartido()).Contains(escolhaVoto))
+                            {
+                                throw new Exception();
+                            }
+
+
+                            listaCandidato.Where(p => p.getNumeroPartido() == escolhaVoto).FirstOrDefault().incrementarVoto();
+                            //candidato.incrementarVoto();
+                            Console.WriteLine("voto computado com sucesso!!");
+                            Console.ReadKey();
+
+                            break;
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Não foi possível realizar o voto");
+                            Console.ReadKey();
+                            break;
                         }
 
-                        var escolhaVoto = Convert.ToInt32(Console.ReadLine());
-                        listaCandidato.Where(p => p.getNumeroPartido() == escolhaVoto).FirstOrDefault().incrementarVoto();
-                        //candidato.incrementarVoto();
-                        Console.WriteLine("voto computado com sucesso!!");
-                        Console.ReadKey();
-
-                        break;
                     case 6:
                         string resultado = "";
-                        int maiorQtdVotos = 0;
-                        foreach (var item in repositorio.consultarTodosCandidatos())
+                        int posicao = 1;
+                        foreach (var item in repositorio.consultarTodosCandidatos().OrderByDescending(p => p.getQtdVotos()))
                         {
-                            if (item.getQtdVotos() > maiorQtdVotos)
-                            {
-                                maiorQtdVotos = item.getQtdVotos();
-                                resultado = "O Candidato " + item.getNome() + " está liderando com " + item.getQtdVotos() + " votos";
-
-                            }
+                            resultado = posicao + "º)" + "O Candidato (a) " + item.getNome() + " está com o total de  " + item.getQtdVotos() + " votos";
+                            Console.WriteLine(resultado);
+                            posicao++;
                         }
-                        Console.WriteLine(resultado);
                         Console.ReadKey();
-
                         break;
+
                     default:
 
                         Console.WriteLine("opção inválida");
